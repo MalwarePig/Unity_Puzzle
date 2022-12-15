17178d6420ce6aa788152adc807bdca5b4bab9ca
MalwarePig <sergio_lcc92@hotmail.com>
2022-12-14 22:27:32 -0600

    first commit

--- Assets/Scripts/juego.cs
+++ Assets/Scripts/juego.cs
@@ -1,45 +1,67 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class juego : MonoBehaviour
 {
     public Sprite[] Niveles;
 
     public GameObject MenuGanar; //Declaración
-    public GameObject MenuContinuar;//Ventana de mensajes
+
+    public GameObject MenuContinuar; //Ventana de mensajes
 
     public GameObject CamaraPrincipal;
 
     public GameObject PiezaSeleccionada;
 
     public GameObject Cronometro;
+
+    public Text textPoema;
+
     int capa = 1;
+
+    int PoemaAsignado = 0;
+
     public int PiezasEncajadas = 0;
-    string[] Poemas = new string[5] {
-    "Argentina",
-    "Bolivia",
-    "Peru",
-    "Chile",
-    "Colombia" 
-    };
+
+    string[]
+        Poemas =
+            new string[5]
+            {
+                "Eres mi pollito morado 💜",
+                "Seimpre recuerda que te quiero mucho más que tú",
+                "Porque tiene unos ojillos que me miran entornados muy gachones y muy pillos, muy pillos, muy pillos",
+                "Pinche vieja arrogante, flaca escuálida, buenérrima, inmamable, deliciosa, envidioa... Es perfecta.",
+                "Eres más de lo que buscaba y mucho mas de lo que merezco"
+            };
 
     void Start()
     {
-
+        PoemaAsignado = Random.Range(0, 5);
         for (int i = 0; i < 36; i++)
         {
-            GameObject.Find("Pieza (" + i + ")").transform.Find("Puzzle").GetComponent<SpriteRenderer>().sprite = Niveles[PlayerPrefs.GetInt("Nivel")];
+            GameObject
+                .Find("Pieza (" + i + ")")
+                .transform
+                .Find("Puzzle")
+                .GetComponent<SpriteRenderer>()
+                .sprite = Niveles[PlayerPrefs.GetInt("Nivel")];
         }
     }
 
     void Update()
-    {
+    { 
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            RaycastHit2D hit =
+                Physics2D
+                    .Raycast(Camera
+                        .main
+                        .ScreenToWorldPoint(Input.mousePosition),
+                    Vector2.zero);
             if (hit)
             {
                 if (hit.transform.CompareTag("Puzzle"))
@@ -47,8 +69,11 @@ public class juego : MonoBehaviour
                     if (!hit.transform.GetComponent<pieza>().Encajada)
                     {
                         PiezaSeleccionada = hit.transform.gameObject;
-                        PiezaSeleccionada.GetComponent<pieza>().Seleccionada = true;
-                        PiezaSeleccionada.GetComponent<SortingGroup>().sortingOrder = capa;
+                        PiezaSeleccionada.GetComponent<pieza>().Seleccionada =
+                            true;
+                        PiezaSeleccionada
+                            .GetComponent<SortingGroup>()
+                            .sortingOrder = capa;
                         capa++;
                     }
                 }
@@ -66,35 +91,37 @@ public class juego : MonoBehaviour
         if (PiezaSeleccionada != null)
         {
             Vector3 raton = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            PiezaSeleccionada.transform.position = new Vector3(raton.x, raton.y, 0);
+            PiezaSeleccionada.transform.position =
+                new Vector3(raton.x, raton.y, 0);
         }
 
         //Comprobar si se ha completado nivel
         if (PiezasEncajadas == 36)
         {
+            Cronometro.GetComponent<Tiempo>().escribirRecord(); //se comprueba el record
 
-            Cronometro.GetComponent<Tiempo>().escribirRecord();//se comprueba el record
-
-            if (PlayerPrefs.GetString("Propuesta") == "Si")
+            if (
+                PlayerPrefs.GetString("Propuesta") == "Si" //Se muestra propuesta o poemas si ya esta respondido
+            )
             {
+                textPoema.text = Poemas[PoemaAsignado];
                 MenuContinuar.SetActive(true);
             }
             else
             {
                 MenuGanar.SetActive(true);
             }
-
-
-
-
-            MenuGanar.SetActive(true);
-            GameObject.Find("Main Camera").GetComponent<AudioSource>().mute = true;//se mutea el audio de la camara (Gameplay audio)
+            GameObject.Find("Main Camera").GetComponent<AudioSource>().mute =
+                true; //se mutea el audio de la camara (Gameplay audio)
         }
     }
+
     public void SiguienteNivel()
     {
         PlayerPrefs.SetString("Propuesta", "Si");
-        if (PlayerPrefs.GetInt("Nivel") < Niveles.Length - 1)//Si o continuar
+        if (
+            PlayerPrefs.GetInt("Nivel") < Niveles.Length - 1 //Si o continuar
+        )
         {
             PlayerPrefs.SetInt("Nivel", PlayerPrefs.GetInt("Nivel") + 1);
         }
